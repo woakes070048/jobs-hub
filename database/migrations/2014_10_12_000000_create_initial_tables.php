fce1dd24e50ff05cc4953712afe0a1540976b010
Karl Hughes <khughes.me@gmail.com>
2016-12-23 15:39:26 -0600

    Initial commit with migration

---
+++ database/migrations/2014_10_12_000000_create_initial_tables.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInitialTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('email')->unique();
+            $table->json('details')->nullable();
+            $table->string('api_key');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('providers', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('name')->unique();
+            $table->json('details')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('user_providers', function (Blueprint $table) {
+            $table->uuid('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->uuid('provider_id')->unsigned();
+            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->primary(['user_id', 'provider_id']);
+            $table->json('provider_options')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_providers');
+        Schema::drop('users');
+        Schema::drop('providers');
+    }
+}
